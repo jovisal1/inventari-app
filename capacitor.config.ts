8667d16565538b455b7e6cec8e34af813fc9f7d8
chverma <chverma@users.noreply.github.com>
2022-03-21 17:16:44 +0100

    Change app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.iestacio.inventaritic',
   appName: 'inventariApp',
   webDir: 'build',
   bundledWebRuntime: false
